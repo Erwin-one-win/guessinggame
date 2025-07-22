b11605dce7e23213509a0c9c3acf0dd43301567b
Erwin-one-win <347173397@qq.com>
2025-07-22 14:42:43 +0800

    guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+actual=$(ls -1 | wc -l)
 
-function ask_guess {
-    echo "你猜当前目录下有多少个文件？"
-    read guess
+function ask_guess{ 
+echo "Please enter the number of files you guess" 
+read response
 }
 
-file_count=$(ls -1 | wc -l)
-
 ask_guess
-while [[ $guess -ne $file_count ]]
+echo "You guessed number is:$response"
+
+
+while [[ $response -ne $actual  ]]
 do
-    if [[ $guess -lt $file_count ]]; then
-        echo "太低了！再试一次："
-    else
-        echo "太高了！再试一次："
-    fi
-    ask_guess
+     if [[ $response -lt $actual  ]]
+     then
+         echo "Low.Try again "
+     elif [[ $response -gt $actual  ]]
+     then
+         echo "High.Try again "
+      fi
+
+       ask_guess
 done
 
-echo "恭喜你猜对了！当前目录下有 $file_count 个文件。"
+echo "Thanks for entering $response. Congratulate,this is right .End program"
 
